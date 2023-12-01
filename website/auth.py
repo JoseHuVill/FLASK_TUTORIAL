718768dcad1483a730ac08bcaeed4a51c49d5c27
Humberto Villarreal <jhvf2001@gmail.com>
2023-11-30 21:13:30 -0600

    http petitions

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auths = Blueprint('auths', __name__)
 
-@auths.route('/login')
+@auths.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -10,6 +10,23 @@ def login():
 def logout():
     return '<h1>Logout Page</h1>'
 
-@auths.route('/sign-up')
+@auths.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    
+        if len(email) < 4:
+            flash('Email is too short', category='error')
+        elif len(firstName) < 2:
+            flash('Name is too short', category='error')
+        elif password1 != password2:
+            flash('Password do not match', category='error')
+        elif len(password1) < 7:
+            flash('Password is too short', category='error')
+        else:
+            flash('Account created !', category='success')
+        
     return render_template('sign-up.html')
